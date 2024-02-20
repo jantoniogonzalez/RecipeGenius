84b3d48863d6ac8a994df9eb908e63d14b78a54f
Juan <jgonzalez020801@gmail.com>
2024-02-20 13:39:41 -0800

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import RecipeList from './RecipeList/RecipeList';
+import HomePage from './HomePage/HomePage';
+import './App.css'
+import '@fontsource-variable/inter';
+
+function App() {
+
+  return (
+    <>
+      <HomePage />
+    </>
+  )
+}
+
+export default App
